63568dbc35f3097f1f91243cccd64ce99a14dcad
ruhulaminrul <ruhul.ok@gmail.com>
2019-12-28 09:12:33 +0600

    initialize ftont hand

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -16,18 +16,25 @@
     </head>
 
 <body>
-@include('layout.nav');
+    @include('layout.nav');
 
 
+    <div class="wrapper">
+        <div class="container">
+            <div class="row">
 
-@yield('content');
+                @include('layout.sidevar');
+                
+                @yield('content');
+
+            </div>
+        </div>
+        <!--/.container-->
+    </div>
+    <!--/.wrapper-->
 
 
-
-
-
-
-@include('layout.footer');
+    @include('layout.footer');
     <script src="scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
     <script src="scripts/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
     <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
