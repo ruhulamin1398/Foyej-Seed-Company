d5cb47fc0eb2ff914e3870e04bb380241ef2659d
ruhulaminrul <ruhul.ok@gmail.com>
2019-12-28 13:49:51 +0600

    sidebar

--- resources/views/layout/sidevar.blade.php
+++ resources/views/layout/sidevar.blade.php
@@ -3,22 +3,43 @@
         <ul class="widget widget-menu unstyled">
             <li class="active"><a href="index.html"><i class="menu-icon icon-dashboard"></i>Dashboard
                 </a></li>
-            <li><a href="activity.html"><i class="menu-icon icon-bullhorn"></i>News Feed </a>
+
+
+
+            <!-- product start     -->
+            <li><a class="collapsed" data-toggle="collapse" href="#toggleProduct"><i class="menu-icon icon-cog">
+                    </i><i class="icon-chevron-down pull-right"></i><i class="icon-chevron-up pull-right">
+                    </i>Products </a>
+                <ul id="toggleProduct" class="collapse unstyled">
+                    <li><a href="other-login.html"><i class="icon-inbox"></i>Add New </a></li>
+                    <li><a href="other-user-listing.html"><i class="icon-inbox"></i>View Product <b class="label green pull-right">11</b> </a></li>
+                </ul>
             </li>
-            <li><a href="message.html"><i class="menu-icon icon-inbox"></i>Inbox <b class="label green pull-right">
-                        11</b> </a></li>
-            <li><a href="task.html"><i class="menu-icon icon-tasks"></i>Tasks <b class="label orange pull-right">
-                        19</b> </a></li>
+
+            <!-- product end  -->
+
+
+            <!-- Catagory start     -->
+            <li><a class="collapsed" data-toggle="collapse" href="#toggleProductCatagory"><i class="menu-icon icon-tasks">
+                    </i><i class="icon-chevron-down pull-right"></i><i class="icon-chevron-up pull-right">
+                    </i>Catagories </a>
+                <ul id="toggleProductCatagory" class="collapse unstyled">
+                    <li><a href="other-login.html"><i class="icon-inbox"></i>Add New </a></li>
+                    <li><a href="other-user-listing.html"><i class="icon-inbox"></i>View Product <b class="label orange pull-right">11</b> </a></li>
+                </ul>
+            </li>
+
+            <!-- Catagory end  -->
+
         </ul>
         <!--/.widget-nav-->
 
 
         <ul class="widget widget-menu unstyled">
-            <li><a href="ui-button-icon.html"><i class="menu-icon icon-bold"></i> Buttons </a></li>
-            <li><a href="ui-typography.html"><i class="menu-icon icon-book"></i>Typography </a></li>
-            <li><a href="form.html"><i class="menu-icon icon-paste"></i>Forms </a></li>
-            <li><a href="table.html"><i class="menu-icon icon-table"></i>Tables </a></li>
-            <li><a href="charts.html"><i class="menu-icon icon-bar-chart"></i>Charts </a></li>
+            <li><a href="ui-button-icon.html"><i class="menu-icon icon-bold"></i> Reports </a></li>
+            <li><a href="ui-typography.html"><i class="menu-icon icon-book"></i> Customers </a></li>
+            <li><a href="form.html"><i class="menu-icon icon-paste"></i>Suppliers </a></li>
+            <li><a href="charts.html"><i class="menu-icon icon-bar-chart"></i>Cost </a></li>
         </ul>
         <!--/.widget-nav-->
         <ul class="widget widget-menu unstyled">
@@ -26,11 +47,15 @@
                     </i><i class="icon-chevron-down pull-right"></i><i class="icon-chevron-up pull-right">
                     </i>More Pages </a>
                 <ul id="togglePages" class="collapse unstyled">
-                    <li><a href="other-login.html"><i class="icon-inbox"></i>Login </a></li>
+                    <li><a href="other-login.html"><i class="icon-inbox"></i>Users </a></li>
                     <li><a href="other-user-profile.html"><i class="icon-inbox"></i>Profile </a></li>
-                    <li><a href="other-user-listing.html"><i class="icon-inbox"></i>All Users </a></li>
+                    <li><a href="other-user-listing.html"><i class="icon-inbox"></i>Employees </a></li>
                 </ul>
             </li>
+
+            <li><a href="other-login.html"><i class="icon-inbox"></i>Users </a></li>
+            <li><a href="other-user-profile.html"><i class="icon-inbox"></i>Profile </a></li>
+            <li><a href="other-user-listing.html"><i class="icon-inbox"></i>Employees </a></li>
             <li><a href="#"><i class="menu-icon icon-signout"></i>Logout </a></li>
         </ul>
     </div>
