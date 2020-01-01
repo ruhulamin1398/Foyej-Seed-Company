ec8645349813ab9da30b57ff9e0c61cf18935451
ruhulaminrul <43258231+ruhulaminrul@users.noreply.github.com>
2020-01-01 15:33:51 +0600

    add title

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Dashboard</title>
+    <title>Foyej Seed Company </title>
 
     <!-- Custom fonts for this template-->
     <link href="{{asset('file/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
