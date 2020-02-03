2bdd61a6d3b549581cfd6af71f0b6823e6f12ecf
ruhulaminrul <ruhul.ok@gmail.com>
2020-02-03 13:33:23 +0600

    verson info

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>SB Admin 2 - Login</title>
+  <title>Abasas Shop Lambda</title>
 
   <!-- Custom fonts for this template-->
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
